f4bc37b0abd68fe0f271cf9eb6eddfb8f42bf244
Shardul Mulay <mulayshardul99@gmail.com>
2024-02-10 20:26:09 +0530

    completed exercise 3.3 phonebook-backend step3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -25,15 +25,31 @@ let persons = [
   },
 ];
 
+// LANDING PAGE / ROOT PATH
 app.get("/", (req, res) => {
   res.send("<p>Welcome to Phonebook!</p>");
 });
 
+// GET ALL PERSONS
 app.get("/api/persons", (req, res) => {
   console.log(`${req.method} - ${req.url}`);
   res.status(200).json(persons);
 });
 
+// GET PERSON BY ID
+app.get("/api/persons/:id", (req, res) => {
+  const personId = Number(req.params.id);
+  const person = persons.find((p) => p.id === personId);
+  if (!person) {
+    return res.status(404).json({
+      httpStatus: 404,
+      message: "Cannot find the requested person",
+    });
+  }
+  res.status(200).json(person);
+});
+
+//  GET INFO FOR PHONEBOOK
 app.get("/api/info", (req, res) => {
   console.log(`${req.method} - ${req.url}`);
   const phonebookCount = persons.length;
