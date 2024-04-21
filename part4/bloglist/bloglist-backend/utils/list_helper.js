c452979c7d3251774c8f371a15705ab06236c47f
Shardul Mulay <mulayshardul99@gmail.com>
2024-04-21 14:29:13 +0530

    completed exercise 4.4 - helper functions and unit tests, step2

--- part4/bloglist/bloglist-backend/utils/list_helper.js
+++ part4/bloglist/bloglist-backend/utils/list_helper.js
@@ -2,4 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+
+  if (blogs.length === 0) return 0
+
+  const likesArray = blogs.map(blog => {
+    return blog.likes
+  })
+
+  const reducer = (sum, item) => {
+    return sum  + item
+  }
+
+  return likesArray.reduce(reducer, 0)
+}
+
+module.exports = { dummy, totalLikes }
